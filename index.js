b30722cf8aeb03e23bcf2cb8de9aafd29d20b5bc
Roma Bobrovskiy <rv.bobrovskiy@gmail.com>
2018-10-11 23:15:19 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	const DATA = [
+		{name: 'Sergei Kolomichenko', student: true, group: 'EK 051-16-1'},
+		{name: 'Bobrovskiy Roman', student: true, group: 'EK 051-16-1'},
+		{name: 'Klokov Alexanrd', student: true, group: 'EK 051-16-1'},
+		{name: 'Daria Chuikina', student: true, group: 'EK 051-16-1'},
+		{name: 'Margo Severinova', student: true, group: 'EK 051-16-1'},
+		{name: 'Irina Barannik', student: true, group: 'EK 051-16-1'},
+		{name: 'Diana Boyko', student: true, group: 'EK 051-16-1'},
+		{name: 'Julia Sadovnikova', student: true, group: 'EK 051-16-1'}
+	];
+	res.status(200).json({message: "Ok", student: DATA});
+});
+
+app.listen(8000, () => {console.log('Listening on 8000')});
